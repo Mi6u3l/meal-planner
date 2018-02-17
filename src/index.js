1afd07a88a803ddf829da121e1c22fe4775a6578
Mi6u3l <miguelbragagomes@gmail.com>
2018-02-17 17:38:26 +0000

    tie up store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
 import { createStore } from 'redux'
 import reducer from './reducers'
 
@@ -11,6 +11,8 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <App store={store} />,
+  document.getElementById('root')
+)
+registerServiceWorker()
