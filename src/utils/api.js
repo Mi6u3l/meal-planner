802218832a19511a15b92a5d8eac303224541c13
Mi6u3l <miguelbragagomes@gmail.com>
2018-02-19 18:04:15 +0000

    add foodlist shoppinglist components

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,8 @@ const APP_KEY = process.env.REACT_APP_APP_KEY
 
 export function fetchRecipes (food = '') {
   food = food.trim()
-
+  console.log('API_ID', API_ID);
+  console.log('API_KEY', APP_KEY);
   return fetch(`https://api.edamam.com/search?q=${food}&app_id=${API_ID}&app_key=${APP_KEY}`)
     .then((res) => res.json())
     .then(({ hits }) => hits.map(({ recipe }) => recipe))
